2f1a2fdc89c0cc12e291edd0a96a9dc7ba8be05c
Edwin Vasquez <edwin.vasquez@grupopdc.com>
2021-06-01 06:36:47 -0600

    se corrige parallax

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
 let mountains_front = document.getElementById('mountains_front');
 
+let btn = document.getElementById('btn');
 let titulo = document.getElementById('titulo');
 let header = document.querySelector('header');
 
@@ -14,5 +15,7 @@ window.addEventListener('scroll', function() {
     mountains_front.style.top = value * 0 + 'px';
 
     titulo.style.marginRight = value * 4 + 'px';
+    titulo.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
     header.style.top = value * 0.5 + 'px';
 });
